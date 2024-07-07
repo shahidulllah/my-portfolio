046087184d60ad35ec87ee40069c10e821340111
shahidullah177 <mdshahidsumon177@gmail.com>
2024-07-08 01:23:22 +0600

    Add real info fixed

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,23 +1,23 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Shahidullah <mdshahidsumon177@gmail.com>',
-      to: ['mdshahidsumon177@gmail.com'],
-      subject: 'Hello world',
-      react: <><p>Email Body</p></>
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'Shahidullah <mdshahidsumon177@gmail.com>',
+//       to: ['mdshahidsumon177@gmail.com'],
+//       subject: 'Hello world',
+//       react: <><p>Email Body</p></>
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
